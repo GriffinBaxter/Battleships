0653dfb81b5c804d4054d957997d658b971bd6ee
Griffin Baxter <grb96@uclive.ac.nz>
2020-10-03 18:53:52 +1300

    Modified some of the code to be more readable and added the ability to select and position 3 differently sizes ships, with a start to IR transmission at the end of the main function.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # File:   Makefile
-# Author: M. P. Hayes, UCECE
-# Date:   12 Sep 2010
+# Author: Mitchell Veale, Griffin Baxter
+# Date:   3 October 2020
 # Descr:  Makefile for game
 
 # Definitions.
@@ -43,9 +43,21 @@ led.o: ../../drivers/led.c ../../drivers/led.h
 navswitch.o: ../../drivers/navswitch.c ../../drivers/avr/delay.h ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/navswitch.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
+ir_uart.o: ../../drivers/avr/ir_uart.c ../../drivers/avr/ir_uart.h ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/avr/timer0.h ../../drivers/avr/usart1.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+usart1.o: ../../drivers/avr/usart1.c ../../drivers/avr/system.h ../../drivers/avr/usart1.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+timer0.o: ../../drivers/avr/timer0.c ../../drivers/avr/bits.h ../../drivers/avr/prescale.h ../../drivers/avr/system.h ../../drivers/avr/timer0.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+prescale.o: ../../drivers/avr/prescale.c ../../drivers/avr/prescale.h ../../drivers/avr/system.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
 
 # Link: create ELF output file from object files.
-game.out: game.o system.o pio.o pacer.o timer.o ledmat.o button.o led.o navswitch.o
+game.out: game.o system.o pio.o pacer.o timer.o ledmat.o button.o led.o navswitch.o ir_uart.o usart1.o timer0.o prescale.o
 	$(CC) $(CFLAGS) $^ -o $@ -lm
 	$(SIZE) $@
 
--- game.c
+++ game.c
@@ -4,32 +4,36 @@
 #include "led.h"
 #include "pacer.h"
 #include "pio.h"
+#include "button.h"
+#include "ir_uart.h"
+
+#define BUTTON_PIO PD7_PIO
 
 /** Define PIO pins driving LED matrix rows.  */
-static const pio_t rows[] =
-        {
-                LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO,
-                LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
-                LEDMAT_ROW7_PIO
-        };
+static const pio_t rows[] = {
+    LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO,
+    LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
+    LEDMAT_ROW7_PIO
+};
 
 
 /** Define PIO pins driving LED matrix columns.  */
-static const pio_t cols[] =
-        {
-                LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
-                LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
-        };
+static const pio_t cols[] = {
+    LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
+    LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
+};
 
-static void display_column(uint8_t row_pattern, uint8_t current_column) {
+static void display_column(uint8_t row_pattern, uint8_t current_column)
+{
     if (current_column == 0) {
         pio_output_high(cols[4]);
     } else {
         pio_output_high(cols[current_column - 1]);
     }
 
-    if(!(row_pattern << 1))
+    if(!(row_pattern << 1)) {
         return;
+    }
 
     for (int i = 0; i < 7; i++) {
         (row_pattern >> i) & 1 ? pio_output_low(rows[i]) : pio_output_high(rows[i]);
@@ -39,38 +43,41 @@ static void display_column(uint8_t row_pattern, uint8_t current_column) {
 
 }
 
-static void placeShip(uint8_t* frame, uint8_t* position, uint8_t length){
+static void placeShip(uint8_t* frame, uint8_t* position, uint8_t length)
+{
     //ship is horizontal
     if (!(*position >> 7)) {
         // is the ship out of the left bound?
-        if ((length == 3 || length == 4) && *position % 5 == 0)
+        if ((length == 3 || length == 4) && *position % 5 == 0) {
             *position+= 1;
-        else if ((length == 2 || length == 3) && *position % 5 == 4)
+        } else if ((length == 2 || length == 3) && *position % 5 == 4) {
             *position-= 1;
-        else if (length == 4 && *position % 5 >= 3)
+        } else if (length == 4 && *position % 5 >= 3) {
             *position -= 3 - (5 - (*position % 5));
-        if (length == 2){
+        }
+        if (length == 2) {
             for (int i = *position % 5; i < length + (*position % 5); i++) {
                 frame[i] |= (1 << (*position / 5));
             }
-        }else{
+        } else {
             for (int i = (*position % 5) - 1; i < length + (*position % 5) - 1; i++) {
                 frame[i] |= (1 << (*position / 5));
             }
         }
-    }else /* Ship is vertical */{
+    } else { /* Ship is vertical */
         // is the ship out of the top bound
-        if ((length == 3 || length == 4) && *position - 128 < 5)
+        if ((length == 3 || length == 4) && *position - 128 < 5) {
             *position += 5;
-        else if ((length == 2 || length == 3) && *position - 128 > 29)
+        } else if ((length == 2 || length == 3) && *position - 128 > 29) {
             *position -= 5;
-        else if (length == 4 && *position - 128 > 24)
+        } else if (length == 4 && *position - 128 > 24) {
             *position -= 5 * (3 - (7 - ((*position - 128) / 5)));
-        if (length == 2){
+        }
+        if (length == 2) {
             for (int i = (*position - 128) / 5; i < ((*position - 128) / 5) + length; i++) {
                 frame[(*position - 128) % 5] |= (1 << i);
             }
-        }else{
+        } else {
             for (int i = ((*position - 128) / 5) - 1; i < length + ((*position - 128) / 5) - 1; i++) {
                 frame[(*position - 128) % 5] |= (1 << i);
             }
@@ -78,66 +85,71 @@ static void placeShip(uint8_t* frame, uint8_t* position, uint8_t length){
     }
 }
 
-static void moveShipUp(uint8_t* frame, uint8_t* position){
+static void moveShipUp(uint8_t* frame, uint8_t* position)
+{
     // first check if we can move the ship
-    for (int i = 0; i < 5; i++){
-        if (frame[i] & 1)
+    for (int i = 0; i < 5; i++) {
+        if (frame[i] & 1) {
             return;
+        }
     }
 
     *position -= 5;
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++) {
         frame[i] >>= 1;
     }
 }
 
-static void moveShipDown(uint8_t* frame, uint8_t* position){
+static void moveShipDown(uint8_t* frame, uint8_t* position)
+{
     // first check if we can move the ship
-    for (int i = 0; i < 5; i++){
-        if (frame[i] & (1 << 6))
+    for (int i = 0; i < 5; i++) {
+        if (frame[i] & (1 << 6)) {
             return;
+        }
     }
 
     *position += 5;
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++) {
         frame[i] <<= 1;
     }
 }
 
-static void moveShipLeft(uint8_t* frame, uint8_t* position){
+static void moveShipLeft(uint8_t* frame, uint8_t* position)
+{
     // first check if we can move the ship
-    if (frame[0] != 0)
+    if (frame[0] != 0) {
         return;
+    }
 
     *position -= 1;
-    for (int i = 1; i < 5; i++){
+    for (int i = 1; i < 5; i++) {
         frame[i-1] = frame[i];
     }
     frame[4] = 0;
 }
 
-static void moveShipRight(uint8_t* frame, uint8_t* position){
+static void moveShipRight(uint8_t* frame, uint8_t* position)
+{
     // first check if we can move the ship
-    if (frame[4] != 0)
+    if (frame[4] != 0) {
         return;
+    }
 
     *position += 1;
-    for (int i = 4; i > 0; i--){
+    for (int i = 4; i > 0; i--) {
         frame[i] = frame[i-1];
     }
     frame[0] = 0;
 }
 
 
-
-int main(void) {
-    system_init();
-    led_init();
-    navswitch_init();
+uint8_t movePlaceShip(int shipLength)
+{
     ledmat_init();
     pacer_init(500);
-
-    led_set(0, 1);
+    
+    //led_set(0, 1);
     uint8_t current_column = 0;
 
     // dim frame
@@ -146,39 +158,76 @@ int main(void) {
     uint8_t frame2[5] = {0, 0, 0, 0, 0};
 
     // first bit is for rotation, second is 0, rest is location
-    uint8_t shipPosition = 30;
+    uint8_t shipPosition = 0b00011110;
 
 
     // first battle ship
-    placeShip(frame2, &shipPosition, 3);
+    placeShip(frame2, &shipPosition, shipLength);
 
     while (1) {
+        system_init();
         pacer_wait();
         navswitch_update();
-        if (navswitch_push_event_p(0))
+        if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
             moveShipUp(frame2, &shipPosition);
+        }
 
-        if (navswitch_push_event_p(2))
+        if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
             moveShipDown(frame2, &shipPosition);
+        }
 
-        if (navswitch_push_event_p(1))
+        if (navswitch_push_event_p(NAVSWITCH_EAST)) {
             moveShipRight(frame2, &shipPosition);
+        }
 
-        if (navswitch_push_event_p(3))
+        if (navswitch_push_event_p(NAVSWITCH_WEST)) {
             moveShipLeft(frame2, &shipPosition);
+        }
 
-        if (navswitch_push_event_p(4)){
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
             shipPosition ^= 1 << 7;
-            for (int i = 0; i < 5; i++){
+            for (int i = 0; i < 5; i++) {
                 frame2[i] = 0;
             }
-            placeShip(frame2, &shipPosition, 3);
+            placeShip(frame2, &shipPosition, shipLength);
         }
 
         current_column++;
-        if (current_column > 4)
+        if (current_column > 4) {
             current_column = 0;
+        }
 
         display_column(frame2[current_column], current_column);
+        
+        if (pio_input_get(BUTTON_PIO)) {
+            break;
+        }
+    }
+    
+    return shipPosition;
+}
+
+
+int main(void)
+{
+    led_init();
+    navswitch_init();
+    pio_config_set(BUTTON_PIO, PIO_INPUT);
+    ir_uart_init();
+
+    uint8_t shipPosition4 = movePlaceShip(4);
+    uint8_t shipPosition3 = movePlaceShip(3);
+    uint8_t shipPosition2 = movePlaceShip(2);
+    
+    uint8_t enemyShipPosition4 = 0;
+    
+    // send and retrieve the first ship position, not working correctly or tested yet
+    while (1) {
+        ir_uart_putc((char) shipPosition4);
+        
+        if (ir_uart_read_ready_p()) {
+            enemyShipPosition4 = ir_uart_getc();
+            break;
+        }
     }
 }
